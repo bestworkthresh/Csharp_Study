1646039c1c2ae0f95981a3ca687e3a0434f39ab7
Thresh1127 <o19931127o@gmail.com>
2022-10-15 00:41:50 +0800

    2022_HOME上傳20221015_HOME_學習進度

---
+++ UsingVariables/UsingVariables/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * UsingVariable
+ * Thresh
+ * 20221014
+ */
+
+
+using System;
+namespace usingvarvables;
+ 
+    class Program
+    {
+            static void Main(string[] args)
+            { 
+                    char aChar = 'a';
+                     Console.WriteLine(aChar);
+
+                    int anInt = 123123;
+                    Console.WriteLine(anInt);
+
+                    //double aDouble ;
+                    //aDouble = 1231.123;
+                    double aDouble = 1231.123;
+                    Console.WriteLine(aDouble);
+
+                    bool aBool = true;
+                    Console.WriteLine(aBool);
+            }
+
+    }
