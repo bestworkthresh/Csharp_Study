51a5349df074dfecdd5acc63fb769a31170768e2
Thresh1127 <o19931127o@gmail.com>
2022-10-15 00:41:50 +0800

    20221015_HOME_學習進度

---
+++ SayHello/SayHello/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * 第二支程式
+ * 時間：20221014 
+ * 作者：Thresh 
+ */
+
+using System;
+namespace consoleApp1 //
+{
+        class Program//物件導向的「類別」
+        {
+                static void Main(string[] args) //
+                {
+                    string message ; //宣告變數
+                    message = "Hello, World!" ;
+                    System.Console.WriteLine(message);       //一個類別，使用WriteLine這個類別
+                }
+        }
+}
+
+
