14a3e81414e149b35c60522df407c6f3dcdaa46d
Thresh1127 <o19931127o@gmail.com>
2022-10-19 22:10:40 +0800

    20221019_HOME_UPDATE

--- UsingVariables/UsingVariables/Program.cs
+++ UsingVariables/UsingVariables/Program.cs
@@ -10,21 +10,38 @@ namespace usingvarvables;
  
     class Program
     {
-            static void Main(string[] args)
-            { 
-                    char aChar = 'a';
-                     Console.WriteLine(aChar);
+        static void Main(string[] args)
+        { 
+        char aChar = 'a';
+        Console.WriteLine(aChar);
 
-                    int anInt = 123123;
-                    Console.WriteLine(anInt);
+        int anInt = 123123;
+        Console.WriteLine(anInt);
 
-                    //double aDouble ;
-                    //aDouble = 1231.123;
-                    double aDouble = 1231.123;
-                    Console.WriteLine(aDouble);
+        //double aDouble ;
+        //aDouble = 1231.123;
+        double aDouble = 1231.123;
+        Console.WriteLine(aDouble);
 
-                    bool aBool = true;
-                    Console.WriteLine(aBool);
-            }
+        bool aBool = true;
+        Console.WriteLine(aBool);
+
+        int x = 256;
+        Console.WriteLine("x："+x);
+
+        int y = 255;
+        Console.WriteLine("y："+y);
+
+        double z = 123.45;
+        Console.Write("z：" + z);
+
+        float f = 123.45f;
+        Console.WriteLine("f：" + f);
+
+        decimal d = 123.45m;
+        Console.WriteLine("d：" + d);
+
 
     }
+
+}
