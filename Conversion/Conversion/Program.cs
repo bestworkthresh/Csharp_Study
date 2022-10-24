9b9c202def2cc63bcd992a80e4c4a8ec7e70b3aa
Thresh1127 <o19931127o@gmail.com>
2022-10-24 13:06:03 +0800

    20221024_FRS_END

--- Conversion/Conversion/Program.cs
+++ Conversion/Conversion/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * UsingVariable
+ * Thresh
+ * 20221023
+ */
+
+
+using System;
+namespace usingvarvables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a  = 10;
+        double b = 0;
+        Console.WriteLine(b = a);
+
+        //Console.WriteLine(a = b);
+
+        b = 20.5;
+        a = (int)b;
+
+        float c = 20;
+        c = 20.5f;
+        c = (float)20.5;
+        Console.WriteLine(c = a);
+
+        char d = (char)65;
+
+
+
+
+    }
+
+}
