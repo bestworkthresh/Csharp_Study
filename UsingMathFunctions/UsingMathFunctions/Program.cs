5d47c1669ce1990503c0e9a081f44138c1b85f91
Thresh1127 <o19931127o@gmail.com>
2022-10-24 23:30:15 +0800

    20221024_HOME_UsingMathFunction

---
+++ UsingMathFunctions/UsingMathFunctions/Program.cs
@@ -0,0 +1,26 @@
+﻿
+/*
+ * UsingMathFunctions
+ * Thresh
+ * 20221024
+ */
+
+
+using System;
+namespace usingvarvables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("sqrt(2) = " + Math.Sqrt(2.0));
+        Console.WriteLine("PI = " + Math.PI);
+        Console.WriteLine("Sin(PI/6.0) = " + Math.Sin(Math.PI / 6.0));
+        Console.WriteLine("Pow(2.0 , 0.5) = " + Math.Pow(2.0, 0.5));
+        Console.WriteLine("Exp(1) = " + Math.Exp(1.0));
+        Console.WriteLine("In (e) = " + Math.Log(Math.E));
+        Console.WriteLine("Log10(100) = " + Math.Log10(100.0));
+
+    }
+
+}
