56d5f2b90e21c44ebdfea1bad911c79522a1576f
Thresh1127 <o19931127o@gmail.com>
2022-10-25 19:21:21 +0800

    20221025_FRS

--- UsingMathFunctions/UsingMathFunctions/Program.cs
+++ UsingMathFunctions/UsingMathFunctions/Program.cs
@@ -16,10 +16,20 @@ class Program
         Console.WriteLine("sqrt(2) = " + Math.Sqrt(2.0));
         Console.WriteLine("PI = " + Math.PI);
         Console.WriteLine("Sin(PI/6.0) = " + Math.Sin(Math.PI / 6.0));
-        Console.WriteLine("Pow(2.0 , 0.5) = " + Math.Pow(2.0, 0.5));
+        Console.WriteLine("Pow(2.0 , 0.5) = " + Math.Pow(2.0, 0.5)); //密次方 
         Console.WriteLine("Exp(1) = " + Math.Exp(1.0));
         Console.WriteLine("In (e) = " + Math.Log(Math.E));
         Console.WriteLine("Log10(100) = " + Math.Log10(100.0));
+
+
+
+
+        //常數
+        const double G = 9.8; //const 可以幫你抓出任何地方要去異動G的值，他就會報錯。
+        Console.WriteLine("常數G = {0} ", G);
+        double t = 1.0;
+        double y = -(1.0/2.0)* G * t * t ;
+        G = 9.8 / 6; //Error! 
 
     }
 
