bd6ecb1ea012532b7f3e7770c59ebd318ba55956
Thresh1127 <o19931127o@gmail.com>
2023-01-17 10:00:25 +0800

    錯誤

---
+++ UsingEnum/UsingEnum/Program.cs
@@ -0,0 +1,29 @@
+﻿//Using enum
+
+
+
+enum WeekDay
+{
+    SUN = 1,
+    MON = 2,
+    TUE = 3,
+    WED = 4,
+    THU = 5,
+    FRI = 6,
+    SAT = 7
+}
+
+
+WeekDay day = WeekDay.TUE;
+switch (day)
+{
+    case WeekDay.SUN:
+        Console.WriteLine("星期日為一周的第{0}天!!", (int)WeekDay.SUN);
+        break;
+    case WeekDay.TUE:
+        Console.WriteLine("星期日為一周的第{0}天!!", (int)WeekDay.TUE);
+        break;
+    default:
+        Console.WriteLine("一周的第{0}天!!", (int)day);
+        break;
+}
--- Using_If/Using_If/Program.cs
+++ Using_If/Using_If/Program.cs
@@ -17,18 +17,39 @@ Console.WriteLine("\n");
 
 Console.WriteLine("請輸入小於100的小數整數(第二種");
 int score = int.Parse(Console.ReadLine());
-//IF的另外一種寫法 
+//IF的另外一種寫法，但藉由true跟false的方式來讓程式操作
 int result = score < 60 ?  50 : score;
 //調分
 Console.WriteLine("調分後分數為"+ result);
 
 
-
 //bool是一個可以儲存判斷結果的定義 
 
 bool x = 3 > 4;
-bool y = 3< 4;
+Console.WriteLine(x+ " = 3 > 4");
 
-Console.WriteLine(x);
-Console.WriteLine(y);
+bool y = 3< 4;
+Console.WriteLine(y+ "= 3< 4");
+
+bool xORy = x | y;
+Console.WriteLine(xORy+ "= x | y");
+
+bool xANDy = x & y;
+Console.WriteLine(xANDy+ "=xANDy");
+
+bool xOy = (x & y) | (x | y);
+Console.WriteLine(xOy+ "= (x & y) | (x | y)");
+
+bool xNy = (x & y) | (x | y);
+Console.WriteLine(xNy+ "= (x & y) | (x | y)");
+
+//Short-Circuit 邏輯運算
+//主要就是為了節省機器運算成本，只要第一個邏輯是False，就不做後面邏輯的判斷
+
+bool xSCx = x && y;
+Console.WriteLine(xSCx);
+
+bool xSC2x = x || y;
+Console.WriteLine(xSC2x);
+//需要考慮到程式執行的副作用，這樣貿然使用Short-Circuit就有可能出現非預期情況
 
